713867177596f4a69ff44009b3d2fde302ec601c
reforzaes <reforza.es@gmail.com>
2026-01-01 14:12:24 +0100

    PruebaCocinas
    Es la prueba de hostinger

---
+++ constants.ts
@@ -0,0 +1,6 @@
+import { IncidentCause, TaskStatus } from './types';
+
+export const SELLERS = ['Lara', 'Maybeth', 'Raquel'];
+export const INSTALLERS = ['Instalador A', 'Instalador B', 'Instalador C', 'Instalador D'];
+export const INCIDENT_CAUSES = Object.values(IncidentCause);
+export const TASK_STATUSES = Object.values(TaskStatus);
